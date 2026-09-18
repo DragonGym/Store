141418c9352d439c3cc1ac54bf0e9991a5a6512a
Jovanyashraf123 <jovanywork12@gmail.com>
2026-09-18 22:04:05 +0300

    s

--- main.js
+++ main.js
@@ -4,14 +4,25 @@ let homeBtn = document.getElementById("home-btn");
 
 let clothes = document.getElementById("CLOTHES1");
 
+let aboutbtn = document.getElementById("About-us")
+
+let aboutsecsion = document.querySelector(".about-us")
+
 
 clothes.addEventListener("click", function() {
 
     home.style.display = "none";
-
+    aboutsecsion.style.display="none";
 });
 
 homeBtn.addEventListener("click", function() {
 
     home.style.display = "block";
+    aboutsecsion.style.display="none";
+
 });
+
+aboutbtn.addEventListener("click",function(){
+    home.style.display="none"
+    aboutsecsion.style.display="block"
+})
